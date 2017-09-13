8946e53fe86719830142f3c94c075646378844f4
Alex-Home <alex.hongquan.xiao@gmail.com>
2017-09-13 00:05:21 -0700

    create upload component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
 import {ServerService} from './service/server.service';
 import {HttpModule} from '@angular/http';
+import { UploadListComponent } from './upload/upload-list/upload-list.component';
+import { UploadFormComponent } from './upload/upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FileUploadComponent
+    UploadListComponent,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule,
