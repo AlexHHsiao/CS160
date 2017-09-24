db3295df1205f48dbd49120654167ceb18f91fbb
Alex-Home <alex.hongquan.xiao@gmail.com>
2017-09-23 21:49:59 -0700

    working on send request to backend

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDwZpOMUQyWBHJhw4bu8wMZKXHoKI_7_ZU',
+    authDomain: 'sjsu-cs-160.firebaseapp.com',
+    databaseURL: 'https://sjsu-cs-160.firebaseio.com',
+    projectId: 'sjsu-cs-160',
+    storageBucket: 'sjsu-cs-160.appspot.com',
+    messagingSenderId: '126660119383'
+  }
 };
