ce577f80bafdf51361d9caf8ce8263dbe2bf754c
Alex-Home <alex.hongquan.xiao@gmail.com>
2017-10-11 23:47:47 -0700

    update

--- web/functions/index.js
+++ web/functions/index.js
@@ -5,6 +5,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+const ffmpegCommand = require('fluent-ffmpeg');
 admin.initializeApp(functions.config().firebase);
 
 
@@ -23,11 +24,34 @@ exports.addMessage = functions.https.onRequest(function (req, res) {
 
 exports.test = functions.https.onRequest(function (req, res) {
 
-  const fileUrl = 'https://firebasestorage.googleapis.com/v0/b/sjsu-cs-160.' +
-    'appspot.com/o/video-org%2Fyizhou.yan92%40gmail.com.avi?alt=media&token=b50afc16-5005-4798-8a7e-0262f87022a1';
-  const file = fs.creat
 
 
+
+
+
+  const fileUrl = 'https://firebasestorage.googleapis.com/' +
+    'v0/b/sjsu-cs-160.appspot.com/o/video-org%2Fyizhou.yan92%40gmail.com.avi?alt=media&token=4e165a2f-21b6-4ee0-bdf8-9ca60f5ecd63';
+
+  //const ff = new ffmpeg('C:/Users/alexh/Desktop/alex/yizhou.yan92@gmail.com.avi');
+
+  var process = new ffmpeg('C:/Users/alexh/Desktop/alex/yizhou.yan92@gmail.com.avi');
+  //var process = new ffmpeg(fileUrl);
+  process.then(function (video) {
+    // Callback mode
+    console.log(video);
+    video.fnExtractFrameToJPG('C:/Users/alexh/Desktop/alexx', {
+      frame_rate : 1,
+      number : 5,
+      file_name : 'my_frame_%t_%s'
+    }, function (error, files) {
+      if (!error)
+        console.log('Frames: ' + files);
+    });
+  }, function (err) {
+    console.log('Error: ' + err);
+  });
+
+  res.send("Hello");
 });
 
 
