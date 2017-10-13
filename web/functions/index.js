cc23cb67ad549e9ec15ddf5f18cc7c6d34d9a507
yizhou yan <yyz2150855@hotmail.com>
2017-10-12 17:18:00 -0700

    express

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,20 +1,67 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+//const functions = require('firebase-functions');
+//const admin = require('firebase-admin');
+const express = require('express')
+const app = express();
+const ffmpeg = require('ffmpeg');
 
 
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello this is http");
+
+app.get('/test', function(req, res) {
+  res.send("Hello World!tttt");
+ // var url = res.url;
+ // console.log(url);
+  try {
+			var process = new ffmpeg('C:/Users/Yizhou/Desktop/1.avi');
+			process.then(function (video) {
+				// Video metadata
+				console.log(video.metadata);
+				// FFmpeg configuration
+				console.log(video.info_configuration);
+			}, function (err) {
+				console.log('Error: ' + err);
+			});
+		} catch (e) {
+			console.log(e.code);
+			console.log(e.msg);
+		}
 });
 
-exports.addMessage = functions.https.onRequest((req, res) => {
-  const original= req.query.text;
 
-  admin.database().ref('/messages').push({original: original}).then(snapshot => {
-    res.redirect(303, snapshot.ref);
-  });
-});
+
+app.listen(3000);
+console.log('Running');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// admin.initializeApp(functions.config().firebase);
+//
+//
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   response.send("Hello this is http");
+// });
+//
+// exports.addMessage = functions.https.onRequest((req, res) => {
+//   const original= req.query.text;
+//
+//   admin.database().ref('/messages').push({original: original}).then(snapshot => {
+//     res.redirect(303, snapshot.ref);
+//   });
+// });
 
 /*exports.getUrl = functions.https.onRequest((req, res) => {
   const user= req.query.text;
--- web/src/app/main/main.component.ts
+++ web/src/app/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ServerService} from '../service/server.service';
 import {AuthService} from '../service/auth.service';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-main',
@@ -11,7 +12,7 @@ export class MainComponent implements OnInit {
 
   a: string;
 
-  constructor(private serverService: ServerService, public authService: AuthService) {
+  constructor(private http:Http, private serverService: ServerService, public authService: AuthService) {
   }
 
   ngOnInit() {
@@ -19,6 +20,9 @@ export class MainComponent implements OnInit {
 
   test() {
 
-    this.serverService.testing(this.a);
+    this.http.get('http://localhost:3000/test').subscribe(
+      (data) => {console.log(data)}
+    )
+
   }
 }
