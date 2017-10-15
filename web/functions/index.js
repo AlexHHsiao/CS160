c5055bc8191a56386ceebee42f609b69099352e4
Alex-Home <alex.hongquan.xiao@gmail.com>
2017-10-15 01:52:16 -0700

    working on ffmpeg

--- web/functions/index.js
+++ web/functions/index.js
@@ -5,32 +5,27 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const ffmpegCommand = require('fluent-ffmpeg');
+const express = require('express');
+const app = express();
 admin.initializeApp(functions.config().firebase);
 
 
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.https.onRequest(function (request, response) {
   response.send("Hello this is http");
 });
 
 exports.addMessage = functions.https.onRequest(function (req, res) {
-  const original= req.query.name;
+  const original = req.query.name;
 
-  admin.database().ref('/messages').push({original: original}).then(snapshot => {
+  admin.database().ref('/messages').push({original: original}).then(function(snapshot) {
     res.redirect(303, snapshot.ref);
-  });
+})
+  ;
 });
 
 exports.test = functions.https.onRequest(function (req, res) {
-
-
-
-
-
-
-  const fileUrl = 'https://firebasestorage.googleapis.com/' +
-    'v0/b/sjsu-cs-160.appspot.com/o/video-org%2Fyizhou.yan92%40gmail.com.avi?alt=media&token=4e165a2f-21b6-4ee0-bdf8-9ca60f5ecd63';
+  const fileUrl = 'https://firebasestorage.googleapis.' +
+    'com/v0/b/sjsu-cs-160.appspot.com/o/video-org%2Fyizhou.yan92%40gmail.com.avi?alt=media&token=9f77b7b1-1a96-4cf4-8009-3485db2185ea';
 
   //const ff = new ffmpeg('C:/Users/alexh/Desktop/alex/yizhou.yan92@gmail.com.avi');
 
@@ -40,9 +35,9 @@ exports.test = functions.https.onRequest(function (req, res) {
     // Callback mode
     console.log(video);
     video.fnExtractFrameToJPG('C:/Users/alexh/Desktop/alexx', {
-      frame_rate : 1,
-      number : 5,
-      file_name : 'my_frame_%t_%s'
+      frame_rate: 1,
+      number: 5,
+      file_name: 'my_frame_%t_%s'
     }, function (error, files) {
       if (!error)
         console.log('Frames: ' + files);
@@ -53,7 +48,6 @@ exports.test = functions.https.onRequest(function (req, res) {
 
   res.send("Hello");
 });
-
 
 
 
