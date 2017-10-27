9dde41173a21aa8482c94eaf150f04baa0a1c2b9
Alex-Mac <alex.hongquan.xiao@gmail.com>
2017-10-27 01:20:08 -0700

    ffmpeg is working now

--- web/functions/frame-extract.js
+++ web/functions/frame-extract.js
@@ -2,22 +2,39 @@ const os = require('os');
 const fs = require('fs');
 const spawn = require('child-process-promise').spawn;
 const gcs = require('@google-cloud/storage')();
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
 
-function extractFrame(fileName) {
+function extractFrame() {
 
-  const sessionId = 'video-org';
-  const fileName = fileName;
+  console.log(parseInt('301/1'));
+  // const promise = spawn('ffprobe', ['-v', 'error', '-of', 'flat=s=_', '-select_streams',
+  //   'v:0', '-show_entries', 'stream=height,width', '/Users/fox/Downloads/test.mp4']);
 
-  const sourceBucketName = 'sjsu-cs-160.appspot.com';
-  const sourceBucket = gcs.bucket(sourceBucketName);
-  const tempDir = os.tmpdir();
+  // ffmpeg -i test.mp4 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p"
 
-  sourceBucket.file(sessionId + '/' + fileName).download({
-      destination: tempDir + '/' + fileName
-    }
-  ).then(() => {
-    console.log('extract frames');
-    console.log(tempDir + '/' + fileName);
-    spawn('ffmpeg', ['-i', tempDir + '/' + fileName, tempDir + '/' + 'frame%04d.png']);
+  const promise = spawn(ffprobePath, ['-v', 'error', '-select_streams', 'v:0', '-show_entries',
+    'stream=avg_frame_rate', '-of', 'default=noprint_wrappers=1:nokey=1', '/Users/fox/Downloads/test.mp4']);
+
+  //const promise = spawn('ffmpeg', ['-i', '/Users/fox/Downloads/test.mp4']);
+
+  const childProcess = promise.childProcess;
+
+  console.log('[spawn] childProcess.pid: ', childProcess.pid);
+  childProcess.stdout.on('data', function (data) {
+    console.log('[spawn] stdout: ', data.toString());
   });
+  childProcess.stderr.on('data', function (data) {
+    console.log('[spawn] stderr: ', data.toString());
+  });
+
+  promise.then(function () {
+    console.log('[spawn] done!');
+  })
+    .catch(function (err) {
+      console.error('[spawn] ERROR: ', err);
+    });
+
 }
+
+extractFrame();
--- web/functions/index.js
+++ web/functions/index.js
@@ -9,6 +9,7 @@ const gcs = require('@google-cloud/storage')();
 const os = require('os');
 const spawn = require('child-process-promise').spawn;
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
 
 admin.initializeApp(functions.config().firebase);
 
@@ -17,8 +18,7 @@ exports.addMessage = functions.https.onRequest(function (req, res) {
 
   admin.database().ref('/messages').push({original: original}).then(function (snapshot) {
     res.redirect(303, snapshot.ref);
-  })
-  ;
+  });
 });
 
 exports.extractFrame = functions.https.onRequest(function (req, res) {
@@ -32,29 +32,52 @@ exports.extractFrame = functions.https.onRequest(function (req, res) {
   const sourceBucket = gcs.bucket(sourceBucketName);
   const temDir = os.tmpdir();
 
+  // res to frontend
+  let fps;
+  let total_frame;
+
   return sourceBucket.file(sessionId + '/' + name).download({
       destination: temDir + '/' + name
     }
   ).then(() => {
-    console.log('extract frames');
+    console.log('read metadata of video');
 
+    const promise = spawn(ffprobePath, ['-v', 'error', '-select_streams', 'v:0', '-show_entries',
+       'stream=avg_frame_rate', '-of', 'default=noprint_wrappers=1:nokey=1', temDir + '/' + name]);
+    const childProcess = promise.childProcess;
+
+    childProcess.stdout.on('data', function (data) {
+      console.log('[spawn] stdout: ', data.toString());
+      fps = parseInt(data.toString());
+    });
+    childProcess.stderr.on('data', function (data) {
+      console.log('[spawn] stderr: ', data.toString());
+    });
+  }).then(() => {
+    console.log('extract frames');
     return spawn(ffmpegPath, ['-i', temDir + '/' + name, temDir + '/' + username + '%d.png']);
   }).then(() => {
     const frames = fs.readdirSync(temDir);
+    total_frame = frames.length - 1;
+    const promiseArray = [];
     console.log(frames);
 
     for (let index in frames) {
       if (index != 0) {
         console.log('uploading');
-        sourceBucket.upload(temDir + '/' + frames[index], {destination: framePath + '/' + frames[index]});
-      }
-
-      if (index == frames.length) {
-        res.send('I am done');
-        return;
+        const p = sourceBucket.upload(temDir + '/' + frames[index], {destination: framePath + '/' + frames[index]});
+        promiseArray.push(p);
       }
     }
+
+    return Promise.all(promiseArray);
+  }).then(() => {
+    console.log('sending res');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+      fps: fps,
+      total_frame: total_frame,
+      email: name
+    }));
   });
-
-
 });
