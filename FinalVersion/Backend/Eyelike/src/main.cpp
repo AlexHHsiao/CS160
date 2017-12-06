8d4efa06c8f1fbd51013c727dceb7135e441febf
yizhou1992 <yyz2150855@hotmail.com>
2017-12-05 20:32:50 -0800

    Update main.cpp

--- FinalVersion/Backend/Eyelike/src/main.cpp
+++ FinalVersion/Backend/Eyelike/src/main.cpp
@@ -7,19 +7,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <sstream>
-//#include <pqxx/pqxx> 
-
-//using namespace std;
-//using namespace pqxx;
-
 #include "constants.h"
 #include "findEyeCenter.h"
 #include "findEyeCorner.h"
 
-//using namespace cv;
 
 /** Constants **/
-
 
 /** Function Headers */
 void detectAndDisplay( cv::Mat frame ,cv::Mat frame_copy,cv::String frame_name);
@@ -43,100 +36,39 @@ int main( int argc, char* argv[] ) { //const char** argv
   std::vector<cv::String> fn;
   cv::glob(path,fn,true); // recurse
 
-// try {
-//      connection C("dbname = testdb user = postgres password = cohondob \
-//      hostaddr = 127.0.0.1 port = 5432");
-//      if (C.is_open()) {
-//         cout << "Opened database successfully: " << C.dbname() << endl;
-//      } else {
-//        cout << "Can't open database" << endl;
-//         return 1;
-//      }
-//      C.disconnect ();
-//  } catch (const std::exception &e) {
-//      cerr << e.what() << std::endl;
-//      return 1;
-//   }
-
-
-
-
-
   // Load the cascades
   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade, please change face_cascade_name in source code.\n"); return -1; };
-
 
   createCornerKernels();
   ellipse(skinCrCbHist, cv::Point(113, 155.6), cv::Size(23.4, 15.2),
           43.0, 0.0, 360.0, cv::Scalar(255, 255, 255), -1);
   
-  // edit begins --->> 
-  // loads a still image from argument --->> 
+  // loads a still image from argument
 
 for (size_t k=0; k<fn.size(); ++k)
 {
-  
-
-     std::cout << fn[k]<< std::endl;
-
+	std::cout << fn[k]<< std::endl;
 	frame = cv::imread(fn[k]);
   
-  // a copy
+  // Make a copy
   	oriFrame = frame;
-
-
-  // flip it --->> 
-  //	cv::flip(frame, frame, 1);
  
-  // copy to debugImage --->> 
-  // dont know yet what this is for --->> 
+  // copy to debugImage 
   	frame.copyTo(debugImage);
 
-  // apply the classifier --->> 
+  // apply the classifier 
   if( !frame.empty() ) {
-
-
-
 
         cv::String outputName = fn[k].substr(fn[k].find_last_of("/") + 1);
 
 	outputName = outputName.substr(0, outputName.find_last_of("."));
 
-
-
-	//cv::String outputName = fn[k].substr(0, fn[k].find_last_of("."));
-
-
-
-
-
-
-
-
-
         detectAndDisplay( frame,oriFrame,outputName);
-
-
-
-
-
-
-
-
-
-
-
-
-
       }
   else {
         printf("-- No image -- Program exits!\n");
         exit(-1);
   }
-
-  
- // releaseCornerKernels();
-
 
 }
  
@@ -213,14 +145,7 @@ void findEyes(cv::Mat frame_gray, cv::Rect face,cv::Mat frame_copy,cv::String fr
   cv::String ff="/home/yizhou/CS160/dlib-19.1/python_examples/frame_eye/" + frame_name+"_processed.jpg";
 
   cv::imwrite (ff, frame_copy);
-  
-//  std::cout << ff << std::endl;
-//  std::cout << rightPupil.y << std::endl;
-//  std::cout << leftPupil.x << std::endl;
-//  std::cout << leftPupil.y << std::endl;
-
-
-
+ 
 
 }
 /**
@@ -237,26 +162,13 @@ void detectAndDisplay( cv::Mat frame,cv::Mat frame_copy,cv::String frame_name) {
 
   for( int i = 0; i < faces.size(); i++ )
   {
-    // to know how many faces detected 
- //   printf("Faces detected: %lu\n", faces.size());
-    // end --->> 
+ 
     rectangle(debugImage, faces[i], 1234);
   }
-  //-- Show what you got
   if (faces.size() > 0) {
-    // for debug ---
-  //  printf("x of rectangle: %d \n", faces[0].x);
-  //  printf("y of rectangle: %d\n", faces[0].y);
-    // end --->> saira
+ 
     findEyes(frame_gray, faces[0],frame_copy,frame_name);
   } else {
-    // if no eyes detected --
-  //   printf("No eyes detected\n");
-    // needs these output if no eyes detected, preferred output than error display --->> saira
- //   std::cout << -1 << std::endl;
-  //  std::cout << -1 << std::endl;
-  //  std::cout << -1 << std::endl;
-  //  std::cout << -1 << std::endl;
     exit(-1);
   }
 }
